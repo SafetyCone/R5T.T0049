d3c236993bb58c7611eb2e66bb967fe1e07d5ed1
davidcoats <david@getsafetycone.com>
2021-09-27 14:42:50 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0049.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0049.Construction
+{
+    /// <summary>
+    /// Construction project for Namespace and Type strongly-typed string types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0049/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0049
+{
+    /// <summary>
+    /// Namespace and Type strongly-typed string types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
