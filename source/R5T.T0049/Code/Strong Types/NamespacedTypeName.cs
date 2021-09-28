856ead36e4b239bae133d4fb393fe188260604de
David Coats <david@getsafetycone.com>
2021-09-28 15:26:53 -0700

    Add NamespacedTypeName strongly-typed string type.

---
+++ source/R5T.T0049/Code/Strong Types/NamespacedTypeName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0049
+{
+    public sealed class NamespacedTypeName : TypedString
+    {
+        #region Static
+
+        public static NamespacedTypeName From(string value)
+        {
+            var output = new NamespacedTypeName(value);
+            return output;
+        }
+
+        #endregion
+
+
+        public NamespacedTypeName(string value)
+            : base(value)
+        {
+        }
+    }
+}
