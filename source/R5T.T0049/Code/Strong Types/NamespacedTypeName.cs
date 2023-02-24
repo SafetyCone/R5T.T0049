1889d5398167a67844d682a624e13a6111dfddbc
davidcoats <david@getsafetycone.com>
2023-02-24 00:22:58 -0800

    Interim changes.

--- source/R5T.T0049/Code/Strong Types/NamespacedTypeName.cs
+++ source/R5T.T0049/Code/Strong Types/NamespacedTypeName.cs
@@ -2,9 +2,12 @@
 
 using R5T.Stagira;
 
+using R5T.T0150;
+
 
 namespace R5T.T0049
 {
+    [StrongTypeMarker]
     public sealed class NamespacedTypeName : TypedString
     {
         #region Static
